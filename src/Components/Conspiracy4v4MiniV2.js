76e83a24b4afb3d33e07092700ebdafca22a5121
Scott Lewis <scottiemlewis1991@gmail.com>
2020-08-25 12:31:03 -0600

    Testing Some Dumb Stuff

--- src/Components/Conspiracy4v4MiniV2.js
+++ src/Components/Conspiracy4v4MiniV2.js
@@ -12,6 +12,7 @@ const mapStateToProps = state => {
 const styles = () => ({
 	matchMini:{
 		height:'100%',
+		width:'100%',
 		display:'flex',
 		'flex-direction':'column',
 		'justify-content':'center',
